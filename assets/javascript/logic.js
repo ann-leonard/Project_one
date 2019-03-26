88d8f6515940c1de1bfc0dbec944805188c0d53e
ann-leonard <aleo0779@gmail.com>
2019-03-25 20:08:49 -0400

    more progress

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,24 +1,30 @@
-
+var filter
 function getResponse(queryURL){
     $.ajax({
-        url: queryURL,
+        url: queryURL
+        ,
         method: "GET"
       }).then(function(response) {
-        
-        
-          for (i=0;i++;i<response.meals.length){
-           category = response.meals[i].strCategory
-         }
-
-        
+    //iterates through response and checks for filters
+        for ( i = 0; i < 6; i++ ){
+            if (filter === response.meals[i].strArea){
+                console.log(response.meals[i])
+            }
+        }
       });
 }
+
+$(".area").on("click", function(){
+    filter = this.innerHTML
+    $(this).removeClass("btn-secondary ")
+    $(this).addClass("btn-primary")
+})
 
 $(document.body).on("click", ".submitBtn", function(){
     var allCategories = "https://www.themealdb.com/api/json/v1/1/list.php?c=list"
     var allAreas = "https://www.themealdb.com/api/json/v1/1/list.php?a=list"
     var userInput = $(".userMealName").val().trim()
-    var queryURL = "https://www.themealdb.com/api/json/v1/1/search.php?s=" + userInput 
+    var queryURL = "https://www.themealdb.com/api/json/v1/1/search.php?s=" + userInput
     if (!userInput){
       userInput = $(".userIngredient").val().trim()
       queryURL = "https://www.themealdb.com/api/json/v1/1/filter.php?i=" + userInput 
@@ -30,113 +36,3 @@ $(document.body).on("click", ".submitBtn", function(){
     $(".userIngredient").val("")
 
 })
-
-
-
-
-// Firebase Authentication //
-//setup guides
-var setupGuides = (data) => {
-
-  let html = "";
-  data.forEach(doc => {
-       const guide = doc.data();
-       console.log(guide);
-       const li =  $("<li>");
-       var div1 = $("<div>");
-       var div2 = $("<div>");
-       div1.addClass("collapsible-header grey lighten-4");
-       div1.text($(guide.title));
-       div2.addClass("collapsible-body white");
-       div2.text($(guide.content));
-       li.append(div1);
-       li.append(div2);
-
-      console.log(li);
-
-      html += li
-       
-      guideList.append(li);
-  });
-  
-}
-
-
-
-
-$("DOMContentLoaded", function () {
-  var modals = $(".modal");
-  M.Modal.init(modals);
-
-  var config = {
-    apiKey: "AIzaSyAxtAWMHjPjjEdZ1gVoGVeSF8i-mdEN9IE",
-    authDomain: "project-one-74297.firebaseapp.com",
-    databaseURL: "https://project-one-74297.firebaseio.com",
-    projectId: "project-one-74297",
-    storageBucket: "project-one-74297.appspot.com",
-    messagingSenderId: "1057793714147"
-  };
-  firebase.initializeApp(config);
-
-  var auth = firebase.auth();
-  var db = firebase.firestore();
-
-
-  //get data
-  db.collection("train").get().then(snapshot => {
-      setupGuides(snapshot.docs)
-  })
-
-  auth.onAuthStateChanged(user => {
-      if (user) {
-          console.log("user logged in: ", user.email);
-      } else {
-          console.log("user logged out");
-      }
-  });
-
-  var signupForm = $("#signup-form");
-  $("#signup-button").on("click", (e) => {
-      e.preventDefault();
-
-      var email = $("#signup-email").val()
-      var password = $("#signup-password").val();
-
-      console.log(email);
-      console.log(password);
-
-      auth.createUserWithEmailAndPassword(email, password).then(cred => {
-          console.log(cred.user);
-          var modal = $("#modal-signup");
-          M.Modal.getInstance(modal).close();
-      });
-  });
-
-  // logout 
-  $("#logout-button").on("click", (e) => {
-      e.preventDefault();
-      firebase.auth().signOut().then(() => {
-          console.log("user signed out");
-      });
-  });
-
-  // login
-  var loginForm = $("#login-form");
-  $("#button-login").on("click", (e) => {
-      e.preventDefault();
-
-      //get user info
-      var email = $("#login-email").val();
-      var password = $("#login-password").val();
-
-      auth.signInWithEmailAndPassword(email, password).then(cred => {
-          console.log(cred.user);
-
-          //close the login modal and reset the form
-          var modal = $("#modal-login");
-          M.Modal.getInstance(modal).close();
-          // loginForm.reset();
-      })
-  })
-
-});
